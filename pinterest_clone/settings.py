793b7e2150d496efcfa52209ac71a740a175e3d6
mrvlkddatta333 <kesavadattakd333@gmail.com>
2023-04-28 07:31:12 +0530

    first commit

--- pinterest_clone/settings.py
+++ pinterest_clone/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'pinterest_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ EMAIL_USE_TLS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "pinterest/static")
 
 # directory that we want to store uploaded files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
